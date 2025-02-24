314b7a3a5d3070e17eaef3c60dfa2366eb0e6fe6
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-02-24 15:33:59 -0300

    The basic login has been added

--- to-do-list/to-do-api/src/auth/auth.service.ts
+++ to-do-list/to-do-api/src/auth/auth.service.ts
@@ -1,10 +1,12 @@
+import { LoginUserDto } from './dto/update-auth.dto';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/register-auth.dto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs'
 import { User } from 'src/users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
@@ -13,12 +15,11 @@ export class AuthService {
     private readonly userRepository: Repository<User>,
     private JwtService: JwtService
   ) { }
-
   async register(registerUserDto: RegisterUserDto): Promise<{ token: string }> {
     const { email, username, password, secondPassword } = registerUserDto
 
     if (password !== secondPassword) {
-
+      throw new BadRequestException(`The passwords does not match`)
 
     }
 
@@ -34,8 +35,26 @@ export class AuthService {
 
     await this.userRepository.save(user)
 
-
     const payload = await this.JwtService.signAsync({ userId: user.id, username: user.username })
+
+    return { token: payload }
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<{ token: string }> {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOneBy({ email })
+
+    if (user == null) {
+      throw new BadRequestException(`The user does not exists`)
+    }
+
+    const passwordEqual = await bcrypt.compare(password, user.password)
+    if (!passwordEqual) {
+      throw new BadRequestException('The password does not match')
+    }
+
+    const payload = await this.JwtService.signAsync({ id: user.id, username: user.username })
 
     return { token: payload }
   }
--- to-do-list/to-do-api/src/auth/dto/update-auth.dto.ts
+++ to-do-list/to-do-api/src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { RegisterUserDto } from './register-auth.dto';
 
-export class UpdateAuthDto extends PartialType(RegisterUserDto) {}
+import { IsEmail, IsNotEmpty, IsString, MinLength, minLength } from "class-validator";
+
+export class LoginUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+}
